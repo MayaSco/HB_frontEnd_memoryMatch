096d6956f2e08a603dc52567180fa57f0abde489
MayaSco <mayaandtyson@gmail.com>
2016-03-21 12:10:10 -0700

    timer/start button functioning w/some "extras"
    fade out first group photo and fade in last photo when all matches are made

--- memoryCard.js
+++ memoryCard.js
@@ -3,6 +3,8 @@
 var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9]; //"images/Tiles/IMG_"
 var deck = [];
 var matches = 0;
+var time = 0;
+var running = false;
 // declare functions
 function newDeck(){
   deck = cards.sort(shuffle);
@@ -10,23 +12,48 @@ function newDeck(){
 function shuffle(){
 	return 0.5 - Math.random();
 }
-
 //Start of game
-	//Timer: New date object when start button is clicked, 
-	//during the game setInterval at every second update the timer element
-	//Subtract old date object, find the number of seconds passed and convert it
-	//Display the number of seconds
-	//Stop timer: Clear interval function
 
 
 //Wrap entire game in a function, call the function(at the end) to start the game
 function init(){
 }
+
+function timer(){
+	// Grab current time, every second get the difference and display OR
+	//Timer: New date object when start button is clicked, 
+	//during the game setInterval at every second update the timer element
+	//Subtract old date object, find the number of seconds passed and convert it
+	//Display the number of seconds
+	//Stop timer: Clear interval function
+	$("#timer-display").html(time);
+	time++;
+	if (running)
+		// use variable for setTimeout method
+		myVar = setTimeout(timer,1000);
+}
+
 // when html page is finshed loading, run this
 $(document).ready(function(){
   newDeck();
+  $(".tile").hide("fast");
+  $(".start-button").click(function(){
+  	$("#group-shot").fadeOut("slow");
+	$(".tile").show(1500);
+
+	if(running){
+		$(".tile").unbind("click");
+		running = false;
+		$(".start-button").html("Start");
+	}else{
+		$(".tile").click(onTileClick);
+		running=true;
+		$(".start-button").html("Stop");
+		timer();
+		}
+	});
   //add event listener when the tiles are clicked using callback onTileClick
-  $(".tile").click(onTileClick);
+  
 });
 // declare this function
 function flipCardOver(){
@@ -43,6 +70,7 @@ function flipCardOver(){
 			// change background image to display nothing 
 			$(myCards).css('background','none');
 			matches++;
+			$("#points-display").html(matches);
 			 //else statement if its not a match to change background image to show back of the card
 			 // used .css to change the image 
 			 // removed face-up class and add the face-down class
@@ -59,9 +87,7 @@ function flipCardOver(){
 		}
 }
 
-
 // declare onTileClick function 
-
 function onTileClick(){
 	// removing card from id and take the substring of the identifier in index 4
 	var i =$(this).attr('id').substring(4); 
@@ -81,49 +107,27 @@ function onTileClick(){
 		
   }
 
-// Select Cards Function
-// Click on one card, to show first card and then second clicked card to check for matches
-	// onclick function (event listener) add flip class to see whether cards match
-
-
-//function selectCard(){
-			//if()
-			//$(this).addClass("card-flipped")
-		//}
-			// do nothing if two cards are already flipped
-			// card is visible true or false
-			//check the pattern of both cards flipped 
-			// set timeout for 10 seconds later
-//}
-
-// Check Cards for Match
-function checkCard(){
-	//convert which card was clicked into an array indice to see if its the same
-			// match is solved true or false 
-			//check to see if cards are a match, if pattern is the same, remove is solved cards from board
-	// set to null     ==null to clear last card clicked to reset to first card clicked
-}
-
-//Remove any matched cards from the board
-function RemoveMatch(){
-
-	//keep tally how many times removeMatch happens
-	//check to see if removeMatch variable tally<9 if so remove card only otherwise remove card and end game
-}
-
 // When all cards are matched
 function celebration(){
 	    // if all cards = is solved then display (animation? and final photo) 
 	    //in raw html/css first so image is on the page display none in css
 	    //show it 
+
+// 	    var $show = $('.show').hide();
+// $('button').on('click', function() {
+//   $show.fadeToggle();
+// });
+	    $("#group-silly").fadeIn("slow");
+	    // stop timer using variable 
+	    clearTimeout(myVar);
 	restartGame();
 }
 
-
-function restartGame(){}
+function restartGame(){
 // End of game
 	//Stop timer: Clear interval function
 	//Display the date "Your time"
 	//Display final photo and a pop up "Congratulations, 'name'!" 
 			// * if API used here, check Firebase to check the scores against the top ranking and compare
 	// "Press start to play again, 'name'!" restart game call setup function (Start of game)
+}
